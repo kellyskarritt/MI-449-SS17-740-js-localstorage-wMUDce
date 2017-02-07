298314233540c5718e11ca068757f7c730d78df5
kellyskarritt <skarritt@msu.edu>
2017-02-07 16:04:31 -0500

    local storage project

---
+++ script.js
@@ -0,0 +1,25 @@
+var time = document.getElementById('time')
+var timeStorage = window.localStorage.getItem('storage')
+function changeClass () {
+  if (time.className === 'day-theme') {
+    time.setAttribute('class', 'night-theme')
+    timeStorage = 'night-theme'
+  } else {
+    time.setAttribute('class', 'day-theme')
+    timeStorage = 'day-theme'
+  }
+}
+
+window.onload = function () {
+  document.getElementById('button').addEventListener('click', changeClass)
+}
+
+window.localStorage.setItem('storage', timeStorage)
+
+var count = window.localStorage.getItem('on_load_counter')
+if (count === null) {
+  count = 0
+}
+count++
+window.localStorage.setItem('on_load_counter', count)
+document.getElementById('display').innerHTML = 'You have visited this site ' + count + ' times.'
